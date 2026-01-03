e6274d5e0e29c6bc117ac64c9a5fa9b23bc6cbf6
Satya Satyajit <satyajitsahoo28252@gmail.com>
2026-01-03 10:48:50 +0530

    changing the cors policy

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,17 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://video-conferencing-platform-nine.vercel.app",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"], // 🔥 REQUIRED
+  })
+);
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
